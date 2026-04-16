efcda6edad90827a608e90e1f57153890238206d
Ahmed Harbi <ahmed.harbi.eg@gmail.com>
2026-04-16 23:51:13 +0200

    fix(infra): correct terraform variable syntax

--- terraform/modules/database/variables.tf
+++ terraform/modules/database/variables.tf
@@ -5,8 +5,14 @@ variable "prefix"              { type = string }
 variable "sku_name"            { type = string }
 variable "storage_mb"          { type = number }
 variable "postgresql_version"  { type = string }
-variable "admin_username"      { type = string; sensitive = true }
-variable "admin_password"      { type = string; sensitive = true }
+variable "admin_username" {
+  type      = string
+  sensitive = true
+}
+variable "admin_password" {
+  type      = string
+  sensitive = true
+}
 variable "delegated_subnet_id" { type = string }
 variable "private_dns_zone_id" { type = string }
 variable "tags"                { type = map(string) }
